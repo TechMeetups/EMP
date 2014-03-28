7d7dd0bb287c2c68a3970ab3e37522883852919b
yuva <yuva@yuva-desktop.(none)>
2014-03-28 16:27:22 +0530

    some changes on active admin

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -14,4 +14,38 @@ ActiveAdmin.register Event do
   #  permitted
   # end
   
+   form do |f|
+    f.inputs "Details" do
+      f.input :user_id
+      f.input :s_date
+      f.input :s_time
+      f.input :e_time
+      f.input :title
+      #f.input :city, :type => :select,:collection => City.all.collect {|a| [a.name, a.id]}
+      
+    end
+    f.actions
+  end
+  permit_params  :user_id, :title, :s_date, :s_time, :e_time
+
+    index do
+   
+    
+    column "Title", :title
+    column "Date", :s_date
+    column "Starting Time", :s_time
+    column "End Time", :e_time
+    
+    actions :defaults => false do |event|
+     link_to "View", admin_event_path(event)
+
+    end
+    actions :defaults => false do |event|
+     link_to "Edit", edit_admin_event_path(event)
+
+    end
+    actions :defaults => false do |event|
+       link_to 'Delete',admin_event_path(event), :confirm => 'Are you sure?', :method => :delete 
+    end
+  end
 end
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register User do
 
+
   
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -13,28 +14,14 @@ ActiveAdmin.register User do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  # index do
-  #   column :city, :type => :select,:collection => City.all.collect {|a| [a.id, a.name]}
-  #   column :email
-  #   column :password
-  #   column :name
-  #   column :company
-  #   column :address
-  #   column :user_type
-  #   column :description
-  #   column :offer
-  #   column :looking_for
-  #   column :twitter
-  #   column :facebook
-  #   column :linkedin
-  #   column :avatar
-  # end
 
 
   form do |f|
     f.inputs "Details" do
-      #f.input :memberships, :label => 'Membership Type', :as => :select, :collection => Membership.all
-      f.input :city, :type => :select,:collection => City.all.collect {|a| [a.name, a.id]}
+
+      #f.input :user_id, :label => 'Member', :as => :select, :collection => User.all.map{|u| ["#{u.last_name}, #{u.first_name}", u.id]}
+      
+      f.input :city_id, :as => :select,:collection => City.all.map {|a| ["#{a.name}",a.id]}
       f.input :email
       f.input :password
       f.input :name
@@ -42,15 +29,39 @@ ActiveAdmin.register User do
       f.input :address
       f.input :user_type
       f.input :description
-      f.input :offer
-      f.input :looking_for
       f.input :twitter
       f.input :facebook
       f.input :linkedin
       f.input :avatar
+
+#City.find(User.find(@event.user_id).city_id).name
       
     end
     f.actions
+
   end
-  permit_params :city, :email, :password, :company, :address, :user_type, :description, :offer, :looking_for, :twitter, :facebook, :linkedin, :avatar, :name
+
+  index do
+    column "Name", :name
+    column "Type", :user_type
+    column "Company", :company
+    column "City" ,:city
+    #  column "City"  do |user|
+    # city_name= City.find(user.city_id).name
+    #  end
+
+    actions :defaults => false do |user|
+     link_to "View", admin_user_path(user)
+
+    end
+    actions :defaults => false do |user|
+     link_to "Edit", edit_admin_user_path(user)
+
+    end
+    actions :defaults => false do |user|
+       link_to 'Delete',admin_user_path(user), :confirm => 'Are you sure?', :method => :delete 
+    end
+  end
+  
+  permit_params :city_id, :email, :password, :company, :address, :user_type, :description, :twitter, :facebook, :linkedin, :avatar, :name
 end
