926d1ba786dee460adad5cb66d86c812901e7a16
yuva <yuva@yuva-desktop.(none)>
2014-04-14 18:40:54 +0530

    config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,18 +27,18 @@ TechMeetsUp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  #  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default :charset => "utf-8"
 
-  ActionMailer::Base.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :authentication => :plain,
-  :domain => 'gmail.com',
-  :user_name => 'ravikantyuvasoft125@gmail.com',
-  :password => 'ravijain'
+  # ActionMailer::Base.smtp_settings = {
+  # :address => "smtp.gmail.com",
+  # :port => 587,
+  # :authentication => :plain,
+  # :domain => 'gmail.com',
+  # :user_name => 'vaibhavyuvasoft122@gmail.com',
+  # :password => 'yuvasoft122'
 }
 end
