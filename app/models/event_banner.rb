5eb46c84d63d926c807be42efbd8416b403cdfd9
yuva <yuva@yuva-desktop.(none)>
2014-04-18 17:21:59 +0530

    Changes in User Model

--- app/models/event_banner.rb
+++ app/models/event_banner.rb
@@ -1,7 +1,7 @@
 class EventBanner < ActiveRecord::Base
 	has_attached_file :file, :styles => { :medium => "500x500" },
 				:storage => :s3,
-                
+                :bucket =>"tmu-emp",
                 :s3_credentials => S3_CREDENTIALS
    	validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/ ,:message => 'Only .jpg/.png/.gif are allowed.'
 	belongs_to :event
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :thumb => "300x300" },
                 :storage => :s3,
-                
-                :s3_credentials => S3_CREDENTIALS
+                :s3_credentials => S3_CREDENTIALS,
+               
+                 :bucket =>"tmu-emp"
             
    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/ ,:message => 'Only .jpg/.png/.gif are allowed.'
   acts_as_taggable # Alias for acts_as_taggable_on :tags
@@ -18,3 +19,13 @@ class User < ActiveRecord::Base
   has_many :event_users , dependent: :destroy
   belongs_to :city
 end
+
+# :storage => :s3,
+# :s3_credentials => "#{Rails.root}/config/s3.yml",
+# :url => ":s3_domain_url",
+# :path => "/:class/avatars/:id.:style.:extension",
+# :bucket =>"ratherism#{Rails.env}",
+# :styles => {
+# :small => ['100x100#', :jpg],
+# :medium => ['250x250', :jpg]
+# }
